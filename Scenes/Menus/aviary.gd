db15df85a21632b4c2bb045704f8cb4e39f72345
allaner <atj3jjj@hotmail.com>
2025-03-06 15:38:28 -0600

    Almost Done with New Store
    Almost have the new store done. Just gotta make sure it is compatible with the rest of the game. Need to make sure it slides in with the aviary menu like normal, and need to implement buying/selecting bird colors still.

--- Scenes/Menus/aviary.gd
+++ Scenes/Menus/aviary.gd
@@ -1,108 +1,28 @@
 extends Control
 
 
-@onready var birdCard1 = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/BirdCard
-@onready var birdCard2 = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer2/BirdCard2
-@onready var birdCard3 = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer3/BirdCard3
+@onready var birdCard1 = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/BirdCard
+@onready var birdCard2 = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/BirdCard2
+@onready var birdCard3 = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/BirdCard3
 @onready var select1 = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Select1
 @onready var select2 = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer2/Select2
 @onready var select3 = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer3/Select3
 
 
 @onready var currentBird = "1"
-@onready var currentBird1 = birdCard1.options.selected
-@onready var currentBird2 = birdCard2.options.selected
-@onready var currentBird3 = birdCard3.options.selected
+#@onready var currentBird1 = birdCard1.options.selected
+#@onready var currentBird2 = birdCard2.options.selected
+#@onready var currentBird3 = birdCard3.options.selected
 
-@onready var player = get_parent().get_parent().get_node("HappyBird")
+#@onready var player = get_parent().get_parent().get_node("HappyBird")
 
 func _ready():
-	birdCard1.Change_Sprite(currentBird1)
-	birdCard2.Change_Sprite(currentBird2)
-	birdCard3.Change_Sprite(currentBird3)
+	pass
 		
 func _process(delta):
 	if self.position.x == 0:
 		Global.mouseHovering = true
 		
-func Bird_Select(x):
-	if x == 1:
-		if Check_Unlocked(x):
-			currentBird = currentBird1
-			select1.text = "Selected"
-			if Global.birdUnlocks2[Global.currentBird2]:
-				select2.text = "Select"
-			if Global.birdUnlocks3[Global.currentBird3]:
-				select3.text = "Select"
-			Global.currentBird = Global.currentBird1
-			player.Change_Bird(player.birddict[Global.currentBird])
-		
-	elif x == 2:
-		if Check_Unlocked(x):
-			currentBird = currentBird2
-			if Global.birdUnlocks1[Global.currentBird1]:
-				select1.text = "Select"
-			select2.text = "Selected"
-			if Global.birdUnlocks3[Global.currentBird3]:
-				select3.text = "Select"
-			Global.currentBird = Global.currentBird2
-			player.Change_Bird(player.birddict[Global.currentBird])
-		
-	elif x == 3:
-		if Check_Unlocked(x):
-			currentBird = currentBird3
-			if Global.birdUnlocks1[Global.currentBird1]:
-				select1.text = "Select"
-			if Global.birdUnlocks2[Global.currentBird2]:
-				select2.text = "Select"
-			select3.text = "Selected"
-			Global.currentBird = Global.currentBird3
-			player.Change_Bird(player.birddict[Global.currentBird])
-	
-	
-func Check_Unlocked(x):
-	if x == 1:
-		if select1.text == "Buy":
-			if Global.mossiesInStock1 >= 100:
-				Global.birdUnlocks1[Global.currentBird1] = true
-				Global.mossiesInStock1 -= 100
-				birdCard1.mossiesLabel.text = str(Global.mossiesInStock1) + "/100"
-				select1.text = "Select"
-				Global.Save_Game()
-		else:
-			return true
-	elif x == 2:
-		if select2.text == "Buy":
-			if Global.mossiesInStock2 >= 250:
-				Global.birdUnlocks2[Global.currentBird2] = true
-				Global.mossiesInStock2 -= 250
-				birdCard2.mossiesLabel.text = str(Global.mossiesInStock1) + "/250"
-				select2.text = "Select"
-				Global.Save_Game()
-		else:
-			return true
-	elif x == 3:
-		if select3.text == "Buy":
-			if Global.mossiesInStock3 >= 625:
-				Global.birdUnlocks3[Global.currentBird3] = true
-				Global.mossiesInStock3 -= 625
-				birdCard3.mossiesLabel.text = str(Global.mossiesInStock1) + "/625"
-				select3.text = "Select"
-				Global.Save_Game()
-		else:
-			return true
-	
-func _on_select_1_pressed():
-	Bird_Select(1)
-	
-
-func _on_select_2_pressed():
-	Bird_Select(2)
-
-
-func _on_select_3_pressed():
-	Bird_Select(3)
-
 
 func _on_back_pressed():
 	get_parent().Menu_Close(get_parent().aviary,get_parent().mainMenu)
--- Scenes/Menus/bird_card.gd
+++ Scenes/Menus/bird_card.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@export var cardNumber : int
+@export var cardName : String
 @export var sprite1 : PackedScene
 @export var sprite2 : PackedScene
 @export var sprite3 : PackedScene
@@ -8,60 +10,172 @@ extends Control
 @export var sprite6 : PackedScene
 @export var sprite7 : PackedScene
 
+@onready var birdName = $PanelContainer/VBoxContainer/PanelContainer/Name
+@onready var carousel = $"Carousel"
+@onready var rightButt = $"PanelContainer/VBoxContainer/HBoxContainer2/RIght Button"
+@onready var leftButt = $"PanelContainer/VBoxContainer/HBoxContainer2/Left Button"
+@onready var buyButt = $"PanelContainer/VBoxContainer/HBoxContainer2/Buy Button"
+@onready var spacerLeft = $PanelContainer/VBoxContainer/HBoxContainer2/spacer
+@onready var spacerRight = $PanelContainer/VBoxContainer/HBoxContainer2/spacer2
 
 @onready var sprites = [
 	sprite1,sprite2,sprite3,sprite4,sprite5,sprite6,sprite7
 ]
 
-var carouselPos = {
-	0: Vector2(860,540),
-	1: Vector2(960,540),
-	2: Vector2(1060,540)
+
+var birdNames = {
+	1: 'blackBird',
+	2: 'redBird',
+	3: 'greenBird',
+	4: 'pinkBird',
+	5: 'yellowBird',
+	6: 'blueBird',
+	7: 'purpleBird'
 }
 
-var carouselPrimary = ''
 
-var transitionTime := 0.25
+var carouselOrient = {
+	0: Vector2(860,550),
+	1: Vector2(960,550),
+	2: Vector2(1060,550)
+}
+
+var carouselPos = 1
+var transitionTime := 0.5
+
+var vis = Color(1,1,1,1)
+var inVis = Color(1,1,1,0)
+var spacerDefault = Vector2(10,0)
+var spacerExpand = Vector2(57,0)
 
 func _ready():
-	Default_Pos()
-	await(get_tree().create_timer(3).timeout)
-	Carousel_Movement_R()
-	await(get_tree().create_timer(3).timeout)
-	Carousel_Movement_R()
-	await(get_tree().create_timer(3).timeout)
-	Carousel_Movement_R()
+	Compensate_for_Control()
+	birdName.text = cardName
+	leftButt.hide()
+	spacerLeft.set_custom_minimum_size(spacerExpand)
+	
+	Check_Sprite_Number()
 
+func Compensate_for_Control():
+	if cardNumber == 1:
+		carouselOrient[0] = Vector2(511,550)
+		carouselOrient[1] = Vector2(611,550)
+		carouselOrient[2] = Vector2(711,550)
+	
+	elif cardNumber == 3:
+		carouselOrient[0] = Vector2(1209,550)
+		carouselOrient[1] = Vector2(1309,550)
+		carouselOrient[2] = Vector2(1409,550)
+		
 
+func Check_Sprite_Number():
+	if cardNumber == 1:
+		sprites.pop_back()
+		sprites.pop_back()
+	
 
 func Default_Pos():
-	
 	var i1 = sprite1.instantiate()
-	add_child(i1)
-	i1.position = carouselPos[1]
-	carouselPrimary = i1
+	carousel.add_child(i1)
+	i1.position = carouselOrient[1]
 	
 	
 func Carousel_Movement_R():
-	var s1 = carouselPrimary
-	#print(s1)
 	
-	var s1ArrayPos = sprites.bsearch(s1)
-	#print(s1ArrayPos)
+	var s1 = carousel.get_children()[0]
 	
-	var s2 = sprites[s1ArrayPos + 1].instantiate()
-	add_child(s2)
-	s2.position = carouselPos[2]
-	#print(s2)
+	var s2 = sprites[carouselPos].instantiate()
+	carousel.add_child(s2)
+	s2.position = carouselOrient[2]
+	s2.set_self_modulate(Color(1,1,1,0.5))
 	
 	var tween = create_tween()
-	tween.parallel().tween_property(s1, "position", Vector2(860,540), transitionTime)
-	tween.parallel().tween_property(s1, "self_modulate", Color(1,1,1,0), transitionTime)
-	tween.parallel().tween_property(s2, "position", Vector2(960,540), transitionTime)
-	tween.parallel().tween_property(s2, "set_self_modulate", Color(1,1,1,1), transitionTime)
+	tween.parallel().tween_property(s1, "position", carouselOrient[0], transitionTime)
+	tween.parallel().tween_property(s1, "self_modulate", inVis, transitionTime)
+	tween.parallel().tween_property(s2, "position", carouselOrient[1], transitionTime)
+	tween.parallel().tween_property(s2, "self_modulate", vis, transitionTime)
 	tween.set_ease(Tween.EASE_IN_OUT)
 	tween.play()
 	
-	print(get_children())
-	carouselPrimary = s2
+	carouselPos += 1
 	
+	await(get_tree().create_timer(transitionTime).timeout)
+	carousel.get_children()[0].queue_free()
+#
+func Carousel_Movement_L():
+	var s1 = carousel.get_children()[0]
+	
+	var s2 = sprites[carouselPos - 2].instantiate()
+	carousel.add_child(s2)
+	s2.position = carouselOrient[0]
+	s2.set_self_modulate(Color(1,1,1,0.5))
+	
+	var tween = create_tween()
+	tween.parallel().tween_property(s1, "position", carouselOrient[2], transitionTime)
+	tween.parallel().tween_property(s1, "self_modulate", inVis, transitionTime)
+	tween.parallel().tween_property(s2, "position", carouselOrient[1], transitionTime)
+	tween.parallel().tween_property(s2, "self_modulate", vis, transitionTime)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.play()
+	
+	carouselPos -= 1
+	
+	await(get_tree().create_timer(transitionTime).timeout)
+	carousel.get_children()[0].queue_free()
+
+func Check_If_Owned():
+	var currentBird = str(birdNames[carouselPos]) + str(cardNumber)
+	
+	if cardNumber == 1:
+		if Global.birdUnlocks1[currentBird]:
+			buyButt.text = 'Select'
+		else:
+			buyButt.text = 'Cost 100'
+			
+	elif cardNumber == 2:
+		if Global.birdUnlocks2[currentBird]:
+			buyButt.text = 'Select'
+		else:
+			buyButt.text = 'Cost 250'
+			
+	elif cardNumber == 3:
+		if Global.birdUnlocks3[currentBird]:
+			buyButt.text = 'Select'
+		else:
+			buyButt.text = 'Cost 675'
+			
+
+func _on_r_ight_button_pressed():
+	Carousel_Movement_R()
+	Check_If_Owned()
+	
+	if !leftButt.visible:
+		leftButt.show()
+		spacerLeft.set_custom_minimum_size(spacerDefault)
+	
+	if carouselPos == len(sprites):
+		rightButt.hide()
+		spacerRight.set_custom_minimum_size(spacerExpand)
+	
+	rightButt.disabled = true
+	await(get_tree().create_timer(transitionTime).timeout)
+	rightButt.disabled = false
+	
+
+func _on_left_button_pressed():
+	Carousel_Movement_L()
+	Check_If_Owned()
+	
+	if !rightButt.visible:
+		rightButt.show()
+		spacerRight.set_custom_minimum_size(spacerDefault)
+	
+	
+	if carouselPos == 1:
+		leftButt.hide()
+		spacerLeft.set_custom_minimum_size(spacerExpand)
+		
+		
+	leftButt.disabled = true
+	await(get_tree().create_timer(transitionTime).timeout)
+	leftButt.disabled = false
