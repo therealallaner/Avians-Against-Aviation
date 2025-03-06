9470347e5b75934eebf83b72359311f4aceeb1e3
allaner <atj3jjj@hotmail.com>
2025-03-05 21:52:01 -0600

    Started Work on the Carousel
    Started Work on the Carousel. Still a work in progress but I am getting it.

--- Scenes/Menus/bird_card.gd
+++ Scenes/Menus/bird_card.gd
@@ -19,11 +19,18 @@ var carouselPos = {
 	2: Vector2(1060,540)
 }
 
-var carouselOrder = []
+var carouselPrimary = ''
+
+var transitionTime := 0.25
 
 func _ready():
 	Default_Pos()
-
+	await(get_tree().create_timer(3).timeout)
+	Carousel_Movement_R()
+	await(get_tree().create_timer(3).timeout)
+	Carousel_Movement_R()
+	await(get_tree().create_timer(3).timeout)
+	Carousel_Movement_R()
 
 
 
@@ -32,15 +39,29 @@ func Default_Pos():
 	var i1 = sprite1.instantiate()
 	add_child(i1)
 	i1.position = carouselPos[1]
-	carouselOrder.append(sprite1)
-	
-	var i2 = sprite2.instantiate()
-	add_child(i2)
-	i2.position = carouselPos[2]
-	i2.set_self_modulate(Color(1,1,1,0))
-	carouselOrder.append(sprite2)
+	carouselPrimary = i1
 	
 	
-func Carousel_Movement():
-	pass
+func Carousel_Movement_R():
+	var s1 = carouselPrimary
+	#print(s1)
+	
+	var s1ArrayPos = sprites.bsearch(s1)
+	#print(s1ArrayPos)
+	
+	var s2 = sprites[s1ArrayPos + 1].instantiate()
+	add_child(s2)
+	s2.position = carouselPos[2]
+	#print(s2)
+	
+	var tween = create_tween()
+	tween.parallel().tween_property(s1, "position", Vector2(860,540), transitionTime)
+	tween.parallel().tween_property(s1, "self_modulate", Color(1,1,1,0), transitionTime)
+	tween.parallel().tween_property(s2, "position", Vector2(960,540), transitionTime)
+	tween.parallel().tween_property(s2, "set_self_modulate", Color(1,1,1,1), transitionTime)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.play()
+	
+	print(get_children())
+	carouselPrimary = s2
 	
