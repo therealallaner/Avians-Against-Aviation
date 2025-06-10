810474342faadb5e6fe5c04dd1b442a847395637
allaner <atj3jjj@hotmail.com>
2025-06-10 15:05:23 -0500

    Bad Changes Dont Like it
    Bad Changes need to Reset to previous commit.

--- Scripts/HappyBirdsClass.gd
+++ Scripts/HappyBirdsClass.gd
@@ -2,6 +2,12 @@ extends Sprite2D
 class_name Happy_Birds
 
 
+var animController = false
+
+
+func _process(delta):
+	if animController:
+		pass
 
 func Anim_Controller(animPlayer):
 	animPlayer.play("Fly")
